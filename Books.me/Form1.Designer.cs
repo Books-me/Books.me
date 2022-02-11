8281f987b0e4cc31b0d480dd22c1c11660957dd2
b0nk0 <kostadinov0475@gmail.com>
2022-02-11 21:39:59 +0200

    dsds

--- Books.me/Form1.Designer.cs
+++ Books.me/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(352, 233);
+            this.button1.Location = new System.Drawing.Point(463, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
