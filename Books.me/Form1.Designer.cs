08acbcfaea8a6ef5ea5b9ab0c5f76693bc695ab3
Gundi <gyunderov@gmail.com>
2022-02-11 21:59:12 +0200

    testov 2

--- Books.me/Form1.Designer.cs
+++ Books.me/Form1.Designer.cs
@@ -45,11 +45,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 119);
+            this.label1.Location = new System.Drawing.Point(141, 284);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(103, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Аз съм нормалник";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
